8a691e9f421e224db549def936219a154708bf24
Akhil Pandey <akhil.pandey@full-cycle.com>
2019-02-07 17:24:50 +0530

    about-us page and faq page design

--- js/custom.js
+++ js/custom.js
@@ -1,85 +1,92 @@
-// var width;
-// $(window).ready(function(){
-//      width = $(window).width();
-// });
-
-// $('a[href*=#]').click(function (e) {
-//     e.preventDefault();
-
-//     var id = $(this).attr('href');
-
-//     if (width < 768) {
-//         var scrollTo = $(id).offset().top - 0;
-//     }
-//     else
-//     {
-//         var scrollTo = $(id).offset().top - 79;
-//     }
-
-//     $('html,body').animate({
-//         'scrollTop': scrollTo
-//     }, 1000);
-// });
-
-
-
-// $(document).scroll(function () {
-//     highlightSection();
-// });
-
-// function highlightSection() {
-//     // Where's the scroll at?
-//     var currentPosition = $(this).scrollTop();
-
-//     // Remove highlights from all
-//     //$('a[href*=#]').removeClass('active');
-
-//     // Loop over each section
-//     $('.parallax section').each(function () {
-//         // Calculate the start and end position of the section
-//         var sectionStart = $(this).offset().top ;
-//         // alert(sectionStart);
-//         var sectionEnd = sectionStart + $(this).height();
-
-//         // If the current scroll lies between the start and the end of this section
-//         if (currentPosition >= sectionStart  && currentPosition < sectionEnd) {
-//             // Highlight the corresponding anchors
-//             //$('a[href=#' + $(this).attr('id') + ']').addClass('active');
-//         }
-//     });
-// };
-
-
-
-
-
-// // Call on page load too!
-// highlightSection();
-
-
-
 $(function() {
-    $(window).on('scroll',function(){
-        var WindowTop = $(window).scrollTop();
-        $('.section, .slidingWrap').each(function(i){
-            if(WindowTop > $(this).offset().top - 79 && 
-               WindowTop < $(this).offset().top + $(this).outerHeight(true)
-              ){
-                $('#menu li a, .subNavWrap a').removeClass('active');
-               $('#menu li, .subNavWrap').eq(i).find('a').addClass('active');
-            }
-        });
-    });
-        $('a[href*=#]:not([href=#])').click(function() {
-         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-           var target = $(this.hash);
-           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-           }
-         }
-        });    
+	
+	// parallax animation
+	    $(window).on('scroll',function(){
+	        var WindowTop = $(window).scrollTop();
+	        $('.section, .slidingWrap').each(function(i){
+	            if(WindowTop > $(this).offset().top - 79 && 
+	               WindowTop < $(this).offset().top + $(this).outerHeight(true)
+	              ){
+	               $('#menu li a, .subNavWrap li a').removeClass('active');
+	               $('#menu li, .subNavWrap li').eq(i).find('a').addClass('active');
+	            }
+	        });
+	        $('.slidingWrap').each(function(i){
+	            if(WindowTop > $(this).offset().top - 79 && 
+	               WindowTop < $(this).offset().top + $(this).outerHeight(true)
+	              ){
+	               $('.subNavWrap li a').removeClass('active');
+	               $('.subNavWrap li').eq(i).find('a').addClass('active');
+	            }
+	        });
+	    });
+	    $('a[href*=#]:not([href=#])').click(function() {
+	     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	       var target = $(this.hash);
+	       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	       if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	       }
+	     }
+	    }); 
+	    // end
+
+        // animation header menu mobile
+			$("#menu a").click(function() {
+			    $("#menu a").removeClass("active");
+			    $(this).addClass("active");
+			});
+			$(".toggleIcon").click(function() {
+			    $('.contactUsBtn').toggleClass('show');
+			    $('body').toggleClass('overflow-hidden');
+			    $('.rightNavLinks').toggleClass('show');
+			    $(this).toggleClass('goRight');
+			    $('.rightNavLinks ul').toggleClass('fadeInUp');
+			});
+        // end
+       
+          
+		// owl carosle initialising function
+			 var owl = $('.owl-carousel');
+		        owl.owlCarousel({
+		            margin: 0,
+		            loop: false,
+		            animateOut: 'fadeOut',
+		            responsive: {
+		                0: {
+		                    items: 1
+		                },
+		                600: {
+		                    items: 1
+		                },
+		                1000: {
+		                    items: 1
+		                }
+		            }
+		        })
+
+		        // for about us page
+		        	var owl = $('.empoweringCompaniesWrap .owl-carousel');
+		                owl.owlCarousel({
+		                    margin: 0,
+		                    loop: false,
+		                    animateOut: 'fadeOut',
+
+		                    responsive: {
+		                        0: {
+		                            items: 1
+		                        },
+		                        600: {
+		                            items: 1
+		                        },
+		                        1000: {
+		                            items: 1
+		                        }
+		                    }
+		               	})
+		        // end
+
       });
